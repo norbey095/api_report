d75c9528f272e2192c084caf379c4c9866267fca
norbey095 <norbey095@gmail.com>
2024-09-30 16:49:13 -0500

    Base project

---
+++ src/main/java/com/emazon/api_report/ApiReportApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.api_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emazon/api_report/ApiReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emazon.api_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
